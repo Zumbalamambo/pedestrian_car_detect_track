11a51872e99ea8be1b1a4d50661b45420a447c38
nu1ptr <j6quach@gmail.com>
2017-06-15 19:21:53 -0700

    Working implementation, no box merging yet

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 import tiny_yolo_net
 import tensorflow as tf
 import cv2
-from keras import backend as K
+import keras
+import matplotlib.pyplot as plt
 
 # Define Flags
 flags = tf.app.flags
@@ -11,24 +12,63 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('video', None, 'Video file to run through the network')
 flags.DEFINE_string('image', None, 'Image file to run through the network')
 
+def load_weights(model,yolo_weight_file):
+    data = np.fromfile(yolo_weight_file,np.float32)
+    data=data[4:]
+
+    index = 0
+    for layer in model.layers:
+        shape = [w.shape for w in layer.get_weights()]
+        if shape != []:
+            kshape,bshape = shape
+            bia = data[index:index+np.prod(bshape)].reshape(bshape)
+            index += np.prod(bshape)
+            ker = data[index:index+np.prod(kshape)].reshape(kshape)
+            index += np.prod(kshape)
+            layer.set_weights([ker,bia])
+
+
 # Will load and image and pre-process it for yolo_net
 def proc_load_image(im, shape):
+    image = cv2.cvtColor(cv2.imread(im), cv2.COLOR_BGR2RGB)
     image = cv2.imread(im)
     image = cv2.resize(image, shape)
+    image = np.transpose(image, (2,0,1))
+    image = 2*(image/255.0) - 1
     image = np.expand_dims(image, axis=0)
     return image
+
+def draw_box(boxes,im):
+    imgcv = im
+
+    for b in boxes:
+        h, w, _ = imgcv.shape
+        bx = b[0]; by = b[1]; bw = b[2]; bh = b[3];
+
+        left  = int ((bx - bw/2.) * w)
+        right = int ((bx + bw/2.) * w)
+        top   = int ((by - bh/2.) * h)
+        bot   = int ((by + bh/2.) * h)
+        if left  < 0    :  left = 0
+        if right > w - 1: right = w - 1
+        if top   < 0    :   top = 0
+        if bot   > h - 1:   bot = h - 1
+        thick = int((h + w) // 150)
+
+        cv2.rectangle(imgcv, (left, top), (right, bot), (255,0,0), thick)
+
+    return imgcv
 
 # Run the network here
 if __name__ == "__main__":
     # Make the Session
-    sess = tf.Session()
-    K.set_session(sess)
 
     # Set the network
+    keras.backend.set_image_dim_ordering('th')
     print('Instantiating Tiny Yolo Net...')
-    yolo = tiny_yolo_net.TinyYoloNet(sess)
-    yolo.net.summary()
-    yolo.set_weights('./weights/yolo-tiny.weights')
+    yolo = tiny_yolo_net.TinyYoloNet()
+    #yolo.set_weights('./weights/yolo-tiny.weights')
+    load_weights(yolo.net, './weights/yolo-tiny.weights')
 
     """
     if FLAGS.video != None:
@@ -42,6 +82,20 @@ if __name__ == "__main__":
     """
 
     # Test
-    test_im = proc_load_image('./data/1.jpg', (448,448))
+    #proc_im = proc_load_image('./data/test1.jpg', (448,448))
+    imagePath = './data/1.jpg'
+    image = plt.imread(imagePath)
 
-    boxes = yolo.process(test_im)
+    resized = cv2.resize(image,(448,448))
+    batch = np.transpose(resized,(2,0,1))
+    batch = 2*(batch/255.) - 1
+    batch = np.expand_dims(batch, axis=0)
+    proc_im = batch
+
+    im = cv2.imread('./data/test1.jpg')
+    boxes = yolo.process(proc_im)
+
+    print(boxes)
+
+    test_im = draw_box(boxes,resized)
+    plt.imsave('out.jpg', test_im)
--- tiny_yolo_net.py
+++ tiny_yolo_net.py
@@ -1,10 +1,15 @@
+#############################################################################################################
+# Referenced this to implement yolonet https://github.com/xslittlegrass/CarND-Vehicle-Detection
+#############################################################################################################
 import numpy as np
 import tensorflow as tf
 import cv2
 import keras
+import matplotlib.pyplot as plt
+from functools import reduce
 from keras.layers.core import Flatten, Dense, Activation, Reshape
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.models import Sequential
 from keras import backend as K
 
@@ -34,118 +39,150 @@ def iou(x1, y1, w1, h1, x2, y2, w2, h2):
 
 # Using Tiny Yolo Net Architecture for pedestrian and car detection
 class TinyYoloNet:
-    # Using TensorFlow Backend for dimensions
-    def __init__(self, sess):
+    def __init__(self):
         # Output Parameters
         self.grid_len = 7           # Length of X and Y axis for grid boxes
         self.num_classes = 20       # Number of classes from the trained VOC
-        self.num_conf_per_box = 2   # Confidences per box
+        self.num_boxes = 2   # Confidences per box
 
-        self.grid_cells = self.grid_len * self.grid_len                     # Should be 49
-        self.probability_len = self.grid_cells * self.num_classes           # Should be 980, 20 class probabilities per grid cell
-        self.confidence_len = self.grid_cells * self.num_conf_per_box       # Should be 98, 2 Confidences per grid cell
-        self.boxes_len = 1470 - self.probability_len - self.confidence_len  # Remaining are box coordinates
+        self.num_grid_cells = self.grid_len * self.grid_len                     # Should be 49
+        self.probability_len = self.num_grid_cells * self.num_classes           # Should be 980, 20 class probabilities per grid cell
+        self.confidence_len = self.num_grid_cells * self.num_boxes              # Should be 98, 2 Confidences per grid cell
+        self.boxes_len = 1470 - self.probability_len - self.confidence_len      # Remaining are box coordinates
 
         # Set the graph
-        self.graph = sess.graph
-        self.sess = sess
-        K.set_session(self.sess)
+        # Construct the model
+        keras.backend.set_image_dim_ordering('th')
+        self.net = Sequential()
 
-        with self.graph.as_default():
-            # Construct the model
-            self.net = Sequential()
+        # Convolutional Layer 1, 16 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(16,3,3, input_shape=(3,448,448), border_mode='same', subsample=(1,1)))
+        self.net.add(LeakyReLU(alpha=0.1))
+        self.net.add(MaxPooling2D(pool_size=(2,2)))
 
-            # Convolutional Layer 1, 16 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(16,(3,3), input_shape=(448,448,3), padding='same', strides=(1,1)))
-            self.net.add(LeakyReLU(alpha=0.1))
-            self.net.add(MaxPooling2D(pool_size=(2,2)))
+        # Convolutional Layer 2, 32 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(32,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
+        self.net.add(MaxPooling2D(pool_size=(2,2), border_mode='valid'))
 
-            # Convolutional Layer 2, 32 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(32,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
-            self.net.add(MaxPooling2D(pool_size=(2,2), padding='valid'))
+        # Convolutional Layer 3, 64 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(64,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
+        self.net.add(MaxPooling2D(pool_size=(2,2), border_mode='valid'))
 
-            # Convolutional Layer 3, 64 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(64,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
-            self.net.add(MaxPooling2D(pool_size=(2,2), padding='valid'))
+        # Convolutional Layer 4, 128 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(128, 3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
+        self.net.add(MaxPooling2D(pool_size=(2,2), border_mode='valid'))
 
-            # Convolutional Layer 4, 128 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(128, (3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
-            self.net.add(MaxPooling2D(pool_size=(2,2), padding='valid'))
+        # Convolutional Layer 5, 256 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(256,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
+        self.net.add(MaxPooling2D(pool_size=(2,2), border_mode='valid'))
 
-            # Convolutional Layer 5, 256 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(256,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
-            self.net.add(MaxPooling2D(pool_size=(2,2), padding='valid'))
+        # Convolutional Layer 6, 512 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(512,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
+        self.net.add(MaxPooling2D(pool_size=(2,2), border_mode='valid'))
 
-            # Convolutional Layer 6, 512 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(512,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
-            self.net.add(MaxPooling2D(pool_size=(2,2), padding='valid'))
+        # Convolutional Layer 7, 1024 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(1024,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
 
-            # Convolutional Layer 7, 1024 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(1024,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
+        # Convolutional Layer 8, 1024 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(1024,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
 
-            # Convolutional Layer 8, 1024 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(1024,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
+        # Convolutional Layer 9, 1024 Feature Maps, (3,3) Kernel
+        self.net.add(Convolution2D(1024,3,3, border_mode='same'))
+        self.net.add(LeakyReLU(alpha=0.1))
 
-            # Convolutional Layer 9, 1024 Feature Maps, (3,3) Kernel
-            self.net.add(Conv2D(1024,(3,3), padding='same'))
-            self.net.add(LeakyReLU(alpha=0.1))
+        # FC Layers
+        self.net.add(Flatten())
 
-            # FC Layers
-            self.net.add(Flatten())
+        # FC Layer 10, 256 Nodes
+        self.net.add(Dense(256))
 
-            # FC Layer 10, 256 Nodes
-            self.net.add(Dense(256))
+        # FC Layer 11, 4096 Nodes
+        self.net.add(Dense(4096))
+        self.net.add(LeakyReLU(alpha=0.1))
 
-            # FC Layer 11, 4096 Nodes
-            self.net.add(Dense(4096))
-            self.net.add(LeakyReLU(alpha=0.1))
-
-            # FC Layer 12, 1470 Nodes
-            self.net.add(Dense(1470))
+        # FC Layer 12, 1470 Nodes
+        self.net.add(Dense(1470))
         return
 
     # Get the weights from https://pjreddie.com/darknet/yolo/
     def set_weights(self, weight_file):
+        # Output Parameters
         print('Loading weights from %s...' % weight_file)
         np_data = np.fromfile(weight_file, np.float32)
         np_data = np_data[4:]
 
         # Just in case
-        K.set_session(self.sess)
-        with self.graph.as_default():
-            # Iterate through the layers and assign each convolutional layer the weights and biases
-            i = 0
-            for layer in self.net.layers:
-                shape_weights = [ w.shape for w in layer.get_weights() ]
+        # Iterate through the layers and assign each convolutional layer the weights and biases
+        i = 0
+        for layer in self.net.layers:
+            shape_weights = [ w.shape for w in layer.get_weights() ]
 
-                # Either an FC Layer or a Conv2D Layer
-                if shape_weights != []:
-                    weight_shape, bias_shape = shape_weights
+            # Either an FC Layer or a Conv2D Layer
+            if shape_weights != []:
+                weight_shape, bias_shape = shape_weights
 
-                    # Reshape the weights into the biases
-                    # Flatten the shapes with np prod to index the weights
-                    bias = np_data[i:i+np.prod(bias_shape)].reshape(bias_shape)
-                    i += np.prod(bias_shape)
-                    weights = np_data[i:i+np.prod(weight_shape)].reshape(weight_shape)
-                    i += np.prod(weight_shape)
-                    layer.set_weights([weights, bias])
+                # Reshape the weights into the biases
+                # Flatten the shapes with np prod to index the weights
+                bias = np_data[i:i+np.prod(bias_shape)].reshape(bias_shape)
+                i += np.prod(bias_shape)
+                weights = np_data[i:i+np.prod(weight_shape)].reshape(weight_shape)
+                i += np.prod(weight_shape)
+                layer.set_weights([weights, bias])
         return
 
     # Process an image
-    def process(self, image):
-        yolo_out = self.net.predict(image)
+    # Output: [[box_info_0], ..., [box_info_n]]
+    # box_info_i = [x,y,w,h,c,prob, label]
+    def process(self, image, threshold_ious=[0.1,0.1], classes=[6,14]):
+        yolo_out = self.net.predict(image)[0]
 
+        # Split the output vector into the corresponding data formats
+        probabilities = yolo_out[0:self.probability_len].reshape((self.num_grid_cells, self.num_classes))
+        confidences = yolo_out[self.probability_len:self.probability_len+self.confidence_len].reshape((self.num_grid_cells, self.num_boxes))
+        box_coordinates = yolo_out[self.probability_len + self.confidence_len:].reshape((self.num_grid_cells, self.num_boxes, 4)) # 4 because 4 values per box
+
+        # Get the boxes
+        boxes = []
+        for grid_cell in range(self.num_grid_cells):
+            for i in range(self.num_boxes):
+                # Grab all confidence value and probability,
+                p = probabilities[grid_cell, :] * confidences[grid_cell,i]
+
+                # Filter out box that doesn't meet threshold
+                threshold_res = [ (label, p[label]) for label, threshold in zip(classes, threshold_ious) if p[label] >= threshold]
+
+                # Sort by probability value in tuple
+                threshold_res.sort(key=lambda x: x[1], reverse=True)
+
+                if threshold_res == []:
+                    continue
+
+                # Get the box
+                boxes.append([
+                    # Grab the coordinate values for the boxes
+                    (box_coordinates[grid_cell, i, 0] + grid_cell % self.grid_len) / self.grid_len,
+                    (box_coordinates[grid_cell, i, 1] + grid_cell // self.grid_len) / self.grid_len,
+                    (box_coordinates[grid_cell, i, 2]) ** 1.8,
+                    (box_coordinates[grid_cell, i, 3]) ** 1.8,
+                    confidences[grid_cell,i],
+                    threshold_res
+                ])
+
+        # Merge boxes now
+        # Must merge with multi-class labels
+
+        return boxes
+
+# Run the network here
 if __name__ == "__main__":
-    sess = tf.Session()
-    K.set_session(sess)
-    yolo = TinyYoloNet(sess)
+    yolo = TinyYoloNet()
     yolo.net.summary()
     yolo.set_weights('./weights/yolo-tiny.weights')
 
